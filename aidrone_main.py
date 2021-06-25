799e74bebd1b76770aef6a54354570b599b878a1
이상락 <64376220+PangRAK@users.noreply.github.com>
2021-06-25 12:22:26 +0900

    coding start

---
+++ aidrone_main.py
@@ -0,0 +1,20 @@
+from time import sleep
+from e_drone.drone import *
+from e_drone.protocol import *
+
+def eventButton(button):
+    print(button.button)
+
+def eventJoystick(joystick):
+    print(joystick.left.x, joystick.left.y, joystick.right.x, joystick.right.y)
+
+if __name__ == '__main__':
+    drone = Drone()
+    drone.open('COM7')
+    drone.setEventHandler(DataType.Joystick, eventJoystick)
+    drone.sendPing(DeviceType.Controller)
+    for i in range(10,0,-1):
+        print(i)
+        sleep(1)
+
+    drone.close()
