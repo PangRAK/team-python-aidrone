8c4b20ea05d900f2be715a6b11489dd15ad4d700
이상락 <64376220+PangRAK@users.noreply.github.com>
2021-06-28 12:45:27 +0900

    commit

--- project_files/aidrone_main.py
+++ project_files/aidrone_main.py
@@ -1,22 +1,58 @@
-from time import sleep
-from e_drone.drone import *
-from e_drone.protocol import *
+from time import sleep                          # 슬립 라이브러리
+from e_drone.drone import *                     # 드론 라이브러리
+from e_drone.protocol import *                  # 드론 라이브러리
+from serial.tools.list_ports import comports    # 포트 번호 가져올 수 있는 라이브러리
+from turtle import *
 
-def eventButton(button):
+def searchPort():                # 포트 검색 함수    
+    # for port, desc, hwid in sorted(comports()):
+    #     print('%s' % (port))
+        
+    nodes = comports()
+    count = 0
+    for node in nodes:
+        print('[{0}]'.format(count))
+        print('device           : ', node.device)
+        print('description      : ', node.description)
+        print('manufacturer     : ', node.manufacturer)
+        print('hwid             : ', node.hwid)
+        print('interface        : ', node.interface)
+        print('location         : ', node.location)
+        print('name             : ', node.name)
+
+def eventButton(button):        # 버튼 입력 정보 함수
     print(button.button)
 
-def eventJoystick(joystick):
+def eventJoystick(joystick):    # 조이스틱 입력 정보 함수
     print(joystick.left.x, joystick.left.y, joystick.right.x, joystick.right.y)
 
+def joystickGoto(joystick):    # 조이스틱 입력 정보 함수
+    goto(joystick.right.x, joystick.right.y)
+
+def eventMotion(motion):
+    print("eventMotion()")
+    print("Accel    :   {0:5},  {1:5},  {2:5}",format(motion.accelX,motion.accelY,motion.accelZ))
+    print("Gyro     :   {0:5},  {1:5},  {2:5}",format(motion.gyroRoll,motion.gyroPitch,motion.gyroYaw))
+    print("Angle    :   {0:5},  {1:5},  {2:5}",format(motion.angleRoll,motion.anglePitch,motion.angleYaw))
+
+
 if __name__ == '__main__':
-    drone = Drone()
-    drone.open('COM7')
-    drone.setEventHandler(DataType.Button, eventButton)
-    drone.sendPing(DeviceType.Controller)
-    drone.setEventHandler(DataType.Joystick, eventJoystick)
-    drone.sendPing(DeviceType.Controller)
-    for i in range(10,0,-1):
+    searchPort()
+    for i in range(5,0,-1):
         print(i)
         sleep(1)
+
+    drone = Drone()
+    drone.open('COM7')                                      # 컨트롤러와 연결된 포트 번호
+    drone.setEventHandler(DataType.Button, eventButton)     # 버튼 이벤트와 연결할 함수를 지정
+    drone.setEventHandler(DataType.Joystick, joystickGoto) # 조이스틱 이벤트와 연결할 함수를 지정
+    drone.setEventHandler(DataType.Motion, eventMotion)
+    drone.sendPing(DeviceType.Controller)
+
+    while True:
+        drone.sendRequest(DeviceType.Drone,DataType.Motion)
+        sleep(1)
+        if KeyboardInterrupt:
+            break;
 
     drone.close()
